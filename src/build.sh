3fa3f20298842d726aff333c1e486db131a166f5
Mariano Guerra <luismarianoguerra@gmail.com>
2009-11-06 17:25:48 -0300

    examples added from roseta code

--- src/build.sh
+++ src/build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 erlc build.erl
-erl -run build build -run init stop | grep -v "{yeccpars" | grep -v "{yy_"
+erl -run build build -run init stop -noshell | grep -v "{yeccpars" | grep -v "{yy_"
