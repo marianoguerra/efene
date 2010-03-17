9113641963e9a6f42219603b2cab5edc6f66f255
Mariano Guerra <luismarianoguerra@gmail.com>
2010-03-17 16:47:13 +0100

    replaced build.erl for build.fn

--- src/compiler/build.sh
+++ src/compiler/build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
-echo "building the erlang modules"
-erlc build.erl
-erl -run build build -run init stop -noshell | grep -v "{yyaction"  | grep -v "{yeccpars" | grep -v "{yy_"
+echo "building efene compiler modules"
+fnc build.fn
+fn build build | grep -v "{yyaction"  | grep -v "{yeccpars" | grep -v "{yy_"
 mv *.beam ../../bin
 rm lexer.erl parser.erl
