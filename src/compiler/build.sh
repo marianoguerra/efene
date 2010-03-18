c759ede3c143af2402ed6035c64dc1a336aa6208
Mariano Guerra <luismarianoguerra@gmail.com>
2010-03-18 23:48:30 +0100

    build the .fn modules using fnc instead of programatically

--- src/compiler/build.sh
+++ src/compiler/build.sh
@@ -2,5 +2,11 @@
 echo "building efene compiler modules"
 fnc build.fn
 fn build build | grep -v "{yyaction"  | grep -v "{yeccpars" | grep -v "{yy_"
+fnc fn_record.fn
+fnc fn_match.fn
+fnc fn_gen.fn
+fnc fn_shell.fn
+fnc fn_info.fn
+fnc efene.fn
 cp *.beam ../../bin
 rm lexer.erl parser.erl
