bbc4edb809d722151ea79b0eb22ef41c3325da53
Mariano Guerra <luismarianoguerra@gmail.com>
2010-04-09 16:48:46 -0300

    added syntax to allow accessing a record field directly and not via the object syntax
    the syntax is record_name.Variable.field
    
    example:
    person.R.firstname

--- src/compiler/build.sh
+++ src/compiler/build.sh
@@ -3,5 +3,5 @@ echo "building efene compiler modules"
 fnc build.fn
 fn build build | grep -v "{yyaction"  | grep -v "{yeccpars" | grep -v "{yy_"
 fnc fn_record.fn fn_match.fn fn_gen.fn fn_shell.fn fn_info.fn efene.fn
-cp *.beam ../../bin
+mv *.beam ../../bin
 rm lexer.erl parser.erl
