1d572cecba69e56c5610ba0f40580968055cb055
Mariano Guerra <luismarianoguerra@gmail.com>
2010-05-24 00:23:53 -0300

    handle enoent while compiling, small cleanup on some libs

--- src/fn_errors.erl
+++ src/fn_errors.erl
@@ -9,6 +9,8 @@ format_error({error, {Line, fn_lexer, [Reason, Cause]}}) ->
     io:format("line ~p: ~s \"~s\"~n", [Line, Reason, Cause]);
 format_error({error, {Line, fn_parser, [Reason, Cause]}}) ->
     io:format("line ~p: ~s \"~s\"~n", [Line, Reason, Cause]);
+format_error({error, enoent}) ->
+    io:format("Error: File doesn't exist~n");
 format_error(Error) ->
     io:format("Error: ~p~n", [Error]).
 
