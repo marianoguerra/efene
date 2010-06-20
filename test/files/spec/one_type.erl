02f1c49d5a8f294fd8b460dcd64696da794f1dd4
Mariano Guerra <luismarianoguerra@gmail.com>
2010-06-20 20:45:06 -0300

    added support for empty list literal in spec

--- test/files/spec/one_type.erl
+++ test/files/spec/one_type.erl
@@ -1,5 +1,5 @@
 -module(one_type).
--export([one_type_1/1, one_type_2/1, one_type_3/1, one_type_4/1]).
+-export([one_type_1/1, one_type_2/1, one_type_3/1, one_type_4/1, one_type_5/1]).
 % test for common type
 -spec one_type_1(integer()) -> integer().
 one_type_1(Int) ->
@@ -19,4 +19,9 @@ one_type_3(4) ->
 -spec one_type_4(<<>>) -> <<>>.
 one_type_4(<<>>) ->
     <<>>.
+
+% test for an empty list literal
+-spec one_type_5([]) -> [].
+one_type_5([]) ->
+    [].
 
