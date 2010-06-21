1d43b69075595d2ec876de8dea316fda7bf75975
Mariano Guerra <luismarianoguerra@gmail.com>
2010-06-20 21:43:35 -0300

    more tests for unions, custom types and literals

--- test/files/spec/unions.erl
+++ test/files/spec/unions.erl
@@ -1,5 +1,5 @@
 -module(unions).
--export([f_1/1, f_2/1]).
+-export([f_1/1, f_2/1, f_3/1, f_4/1]).
 % test simple union of default types
 -spec f_1(integer() | float()) -> integer() | float().
 f_1(Val) ->
@@ -9,5 +9,15 @@ f_1(Val) ->
 -spec f_2(any() | none() | pid() | port() | ref() | float() | integer() | atom() | list () | binary() | fun() | tuple()) ->
     any() | none() | pid() | port() | ref() | float() | integer() | atom() | list () | binary() | fun() | tuple().
 f_2(Val) ->
+    Val.
+
+% test simple union of custom types
+-spec f_3(foo() | bar()) -> foo() | bar().
+f_3(Val) ->
+    Val.
+
+% test simple union of literals
+-spec f_4(0 | false | [] | {} | <<>>) -> 0 | false | [] | {} | <<>>.
+f_4(Val) ->
     Val.
 
