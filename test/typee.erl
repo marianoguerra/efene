926c1028cdd13f41e3e7946be91ce40ab062acd9
Mariano Guerra <luismarianoguerra@gmail.com>
2010-07-09 19:12:25 -0300

    added all_types test to the tests

--- test/typee.erl
+++ test/typee.erl
@@ -9,7 +9,12 @@ params() ->
     tu:test_file("files/type/params.ifn", "files/type/params.erl"),
     ok.
 
+all_erlang_types() ->
+    tu:test_file("files/type/all_types.ifn", "files/type/all_types.erl"),
+    ok.
+
 all() ->
     tu:test(?MODULE, simple),
     tu:test(?MODULE, params),
+    tu:test(?MODULE, all_erlang_types),
     ok.
