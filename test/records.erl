5b6539824429e2c1ce3407e417e73c5840aa1725
Mariano Guerra <luismarianoguerra@gmail.com>
2010-07-23 01:15:51 -0300

    made record tests pass again

--- test/records.erl
+++ test/records.erl
@@ -2,9 +2,9 @@
 -compile(export_all).
 
 simple() ->
-    tu:test_mod_ast("\n\nperson = record(firstname=\"Mariano\", lastname=\"Guerra\", age=24)\n",
+    tu:test_mod_ast("\n\n@rec(person) -> ((firstname, \"Mariano\"), (lastname, \"Guerra\"), (age, 24))\n",
         "-module(temp).\n-export([]).\n-record(person, {firstname=\"Mariano\", lastname=\"Guerra\", age=24}).\n", "temp"),
-    tu:test_mod_ast("\n\nperson = record(firstname=1.2, lastname=false, age=atom)\n",
+    tu:test_mod_ast("\n\n@rec(person) -> ((firstname, 1.2), (lastname, false), (age, atom))\n",
         "-module(temp).\n-export([]).\n-record(person, {firstname=1.2, lastname=false, age=atom}).\n", "temp"),
     ok.
 
