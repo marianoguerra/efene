679e9202b6560784df52b77f15f1448f8dfe5ff2
Mariano Guerra <luismarianoguerra@gmail.com>
2011-05-08 13:55:03 -0300

    allow empty lines and comment lines on the shell

--- src/fn_shell.erl
+++ src/fn_shell.erl
@@ -7,18 +7,31 @@ start() ->
     loop(erl_eval:new_bindings()).
 
 loop(Bindings) ->
-    Expression = io:get_line(">>> "),
-    if Expression /= eof ->
-           try Ast = fn:get_ast(string, Expression),
-               {value, Result, NewBindings} = erl_eval:exprs(Ast,
-                                                             Bindings),
+    OriginalExpression = io:get_line(">>> "),
 
-               fio:println(Result),
-               loop(NewBindings)
-           catch
-             _:Error -> fn_errors:format_error(Error), loop(Bindings)
-           end;
-       true -> io:format("~n"), ok
+    if
+        OriginalExpression /= eof ->
+            Expression = ltrim(OriginalExpression),
+            [FirstChar|_] = Expression,
+
+            if FirstChar == $#; Expression == "\n" ->
+                loop(Bindings);
+            true ->
+                try
+                    Ast = fn:get_ast(string, Expression),
+                    {value, Result, NewBindings} = erl_eval:exprs(Ast, Bindings),
+                    fio:println(Result),
+                    loop(NewBindings)
+                catch
+                    _:Error -> fn_errors:format_error(Error), loop(Bindings)
+                end
+            end;
+
+        true -> io:format("~n"), ok
     end.
 
+ltrim([32|Rest]) ->
+    ltrim(Rest);
 
+ltrim(Result) ->
+    Result.
