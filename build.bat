3fb194654a33c71848cb6f611fad919ffeace1e0
David Mike Simon <david.mike.simon@gmail.com>
2011-10-11 09:20:07 -0700

    Removed old unneeded build scripts, using rebar in root build scripts

--- build.bat
+++ build.bat
@@ -1,3 +1,2 @@
 pushd tools ; make ; popd
-pushd src ; build.bat ; popd
-pushd lib ; build.bat ; popd
+rebar compile
--- build.sh
+++ build.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env sh
 export FNPATH=$(pwd)
 cd tools ; make ; cd ..
-cd src ; ./build.sh ; cd .. 
-cd lib ; ./build.sh ; cd .. 
+./rebar compile
