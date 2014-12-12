69ef3f2ee7802f9cbb4d05893461dd51ce96dbdc
Mariano Guerra <luismarianoguerra@gmail.com>
2014-12-12 21:04:35 +0100

    move macros to hrl file

---
+++ include/efene.hrl
@@ -0,0 +1,15 @@
+% sequence
+-define(S(Line, Type, Val), {seq, Line, Type, Val}).
+% val
+-define(V(Line, Type, Val), {val, Line, Type, Val}).
+% expression
+-define(E(Line, Type, Val), {expr, Line, Type, Val}).
+% operation
+-define(O(Line, Type, Left, Right), {op, Line, Type, Left, Right}).
+% unary operation
+-define(UO(Line, Type, Val), {unary_op, Line, Type, Val}).
+% tag
+-define(T(Line, Tag, Val), {tag, Line, Tag, Val}).
+
+-define(Atom(Val), ?V(_, atom, Val)).
+
--- src/fn_to_erl.erl
+++ src/fn_to_erl.erl
@@ -1,20 +1,7 @@
 -module(fn_to_erl).
 -export([ast_to_ast/2, to_erl/1]).
 
-% sequence
--define(S(Line, Type, Val), {seq, Line, Type, Val}).
-% val
--define(V(Line, Type, Val), {val, Line, Type, Val}).
-% expression
--define(E(Line, Type, Val), {expr, Line, Type, Val}).
-% operation
--define(O(Line, Type, Left, Right), {op, Line, Type, Left, Right}).
-% unary operation
--define(UO(Line, Type, Val), {unary_op, Line, Type, Val}).
-% tag
--define(T(Line, Tag, Val), {tag, Line, Tag, Val}).
-
--define(Atom(Val), ?V(_, atom, Val)).
+-include("efene.hrl").
 
 new_state() -> #{errors => [], warnings => [], attrs => []}.
 
